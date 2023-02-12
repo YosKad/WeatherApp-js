38c7c63666590957af7cf149b69638aa7c5af50b
YosKad <yoskadtl@gmail.com>
2023-02-12 11:17:31 +0200

    upload the weater app

---
+++ scripts/forecast.js
@@ -0,0 +1,34 @@
+// -- adding classes to the weather app project -- //
+
+class Forecast {
+    constructor() {
+        this.key = '1YbJk1U0yTGDKH6cH6zenOjW0zOAdQJA';
+        this.weatherURI = 'http://dataservice.accuweather.com/currentconditions/v1/';
+        this.cityURI = 'http://dataservice.accuweather.com/locations/v1/cities/search';
+    }
+    async updateCity(city) {
+        const cityDets = await this.getCity(city);
+        const weather = await this.getWeather(cityDets.Key);
+
+        // object shorthand notation
+        // when the property name and value its the name we can write like this 
+        return { cityDets, weather };
+    }
+    async getCity(city) {
+        const query = `?apikey=${this.key}&q=${city}`;
+        const response = await fetch(this.cityURI + query);
+        const data = await response.json();
+
+        return data[0];
+    };
+    async getWeather(id) {
+        const query = `${id}?apikey=${this.key}`;
+        const response = await fetch(this.weatherURI + query);
+        const data = await response.json();
+
+        return data[0];
+    }
+}
+
+
+
---
+++ scripts/sandbox.js
@@ -0,0 +1,34 @@
+// --- store data in local storage --- //
+// localStorage.setItem('name' , 'yossef');
+// localStorage.setItem('age' , 30);
+
+// // get data from local storage
+// let name = localStorage.getItem('name');
+// let age = localStorage.getItem('age');
+
+// console.log(name , age)
+
+// // deleting data from local storage
+// // localStorage.removeItem('name')
+
+// localStorage.clear();
+
+// name = localStorage.getItem('name');
+
+// console.log(name);
+
+// --- get to the localstorage arry --- //
+
+// const todos = [
+//     {text: 'play god of war' , author: 'yossef'},
+//     {text: 'take care of my Industrial facility' , author: 'sapir'},
+//     {text: 'play with my parents' , author: 'liness'}
+// ];
+
+// //console.log(JSON.stringify(todos))
+
+// localStorage.setItem('todos' , JSON.stringify(todos));
+
+// const stored = localStorage.getItem('todos');
+
+// console.log(JSON.parse(stored));
